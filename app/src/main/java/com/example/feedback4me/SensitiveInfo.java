598be1cbdc07aa1739b4d191837a414a497d4f61
Andrei Simion <andrei.simion0405@gmail.com>
2019-12-30 13:45:47 +0200

    Changed back for consistency.

---
+++ app/src/main/java/com/example/feedback4me/SensitiveInfo.java
@@ -0,0 +1,12 @@
+package com.example.feedback4me;
+
+public class SensitiveInfo
+{
+    private static String webClientId = "909133630679-f5jdkll5cfptf3baqfp0t8iq4hkorvim.apps.googleusercontent.com";
+
+    public static String getWebClientId()
+    {
+        return webClientId;
+    }
+
+}
